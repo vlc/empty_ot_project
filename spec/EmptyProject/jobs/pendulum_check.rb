45743c85f254664770cbc4c366665aa6fd65b5df
tim.veitch <tim.veitch@1d98dbbe-d728-0410-a43a-8c3833854064>
2012-11-27 08:14:36 +0000

    runs pendulum checker (new commit)

---
+++ spec/EmptyProject/jobs/pendulum_check.rb
@@ -0,0 +1,5 @@
+# OmniTRANS Job for 'EmptyProject'
+# Created 26/11/2012 4:12:48 PM
+require 'D:\BDK\bedrock\app\pendulum\pendulum_warning_emailer'
+
+PendulumWarningEmailer.send_emails('D:\BDK\bedrock\app\pendulum\timesheet_dissect.csv')
